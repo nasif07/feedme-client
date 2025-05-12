c505a77434c3bb5afb501373799931a09af9685a
nasif07 <nasifulislamnasif23@gmail.com>
2025-05-12 15:55:53 +0600

    changes

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { toast } from "react-hot-toast";
 import { Menu, X, ShoppingCart } from "lucide-react";
 import { useAppSelector, useAppDispatch } from "@/redux/hooks";
@@ -30,11 +30,29 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [mounted, setMounted] = useState(false);
 
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [showNavbar, setShowNavbar] = useState(true);
+
   const pathname = usePathname();
 
   useEffect(() => {
     setMounted(true);
   }, []);
+
+  const handleScroll = useCallback(() => {
+    const currentScroll = window.scrollY;
+    if (currentScroll > scrollPosition) {
+      setShowNavbar(false);
+    } else {
+      setShowNavbar(true);
+    }
+    setScrollPosition(currentScroll);
+  }, [scrollPosition]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
 
   const handleLogout = () => {
     dispatch(logout());
@@ -59,208 +77,218 @@ const Navbar = () => {
       link: "/about",
     },
     {
+      name: "How It Works",
+      link: "/how-it-works",
+    },
+    {
       name: "Contact Us",
       link: "/contact",
     },
   ];
 
   return (
-    <nav className="bg-white">
-      <div className="container">
-        <div className="flex items-center justify-between py-3">
-          <div className="flex-shrink-0">
-            <Link href="/" className="text-3xl font-semibold">
-              feedme.
-            </Link>
-          </div>
+    <header className="pb-16">
+      <nav
+        className={`fixed top-0 left-0 z-50 w-full transition-all duration-300 ${
+          scrollPosition > 300 ? "bg-[#D6F599]" : "bg-transparent"
+        } ${showNavbar ? "translate-y-0" : "-translate-y-full"}`}
+      >
+        <div className="container">
+          <div className="flex items-center justify-between py-3">
+            <div className="flex-shrink-0">
+              <Link href="/" className="text-3xl font-semibold">
+                feedme.
+              </Link>
+            </div>
 
-          {/* Desktop Navigation */}
-          <div className="hidden md:flex md:items-center md:gap-6">
-            {navItems.map((navItem, index) => {
-              const isActive = pathname === navItem.link;
-              return (
-                <div key={index} className="group flex flex-col items-center">
-                  <div className="size-1.5" />
-                  <Link
-                    href={navItem.link}
-                    className={`text-[17px] font-medium duration-300 ease-in-out ${
-                      isActive && "text-feed-jungle/60"
-                    } group-hover:text-feed-jungle/50`}
-                  >
-                    {navItem.name}
-                  </Link>
-                  <div
-                    className={`size-1.5 rounded-full duration-300 ease-in-out ${
-                      isActive
-                        ? "bg-feed-jungle/40"
-                        : "group-hover:bg-feed-jungle/40"
-                    }`}
-                  />
-                </div>
-              );
-            })}
+            {/* Desktop Navigation */}
+            <div className="hidden md:flex md:items-center md:gap-6">
+              {navItems.map((navItem, index) => {
+                const isActive = pathname === navItem.link;
+                return (
+                  <div key={index} className="group flex flex-col items-center">
+                    <div className="size-1.5" />
+                    <Link
+                      href={navItem.link}
+                      className={`text-[17px] font-medium duration-300 ease-in-out ${
+                        isActive && "text-feed-jungle/60"
+                      } group-hover:text-feed-jungle/50`}
+                    >
+                      {navItem.name}
+                    </Link>
+                    <div
+                      className={`size-1.5 rounded-full duration-300 ease-in-out ${
+                        isActive
+                          ? "bg-feed-jungle/40"
+                          : "group-hover:bg-feed-jungle/40"
+                      }`}
+                    />
+                  </div>
+                );
+              })}
 
-            {/* Desktop Cart Icon */}
-            <Link href="/cart" className="relative flex items-center">
-              <ShoppingCart className="h-6 w-6 text-gray-700" />
-              {cartItemCount > 0 && (
-                <span className="absolute -top-1 -right-1 flex h-5 w-5 items-center justify-center rounded-full bg-[#FF0000] text-xs text-white">
-                  {cartItemCount}
-                </span>
-              )}
-            </Link>
-
-            {/* Large Device Menu */}
-            <DropdownMenu>
-              <DropdownMenuTrigger className="text-feed-black border-feed-black hover:bg-feed-lime flex h-10 cursor-pointer items-center gap-1 rounded-full border-[1.9px] bg-transparent px-2.5 text-lg font-medium caret-neutral-50 outline-0 duration-300">
-                {user ? (
-                  <>
-                    <MdOutlineAccountCircle className="size-5" />
-                    <span>Account</span>
-                  </>
-                ) : (
-                  <>
-                    <span>Get Started</span>{" "}
-                    <FaArrowRightLong className="ml-1" />
-                  </>
+              {/* Desktop Cart Icon */}
+              <Link href="/cart" className="relative flex items-center">
+                <ShoppingCart className="h-6 w-6 text-gray-700" />
+                {cartItemCount > 0 && (
+                  <span className="absolute -top-1 -right-1 flex h-5 w-5 items-center justify-center rounded-full bg-[#FF0000] text-xs text-white">
+                    {cartItemCount}
+                  </span>
                 )}
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="border-feed-black w-fit space-y-1 rounded-2xl border">
-                <DropdownMenuItem className="bg-feed-lime/50 hover:!bg-feed-jungle group w-ful h-9 rounded-[11px] text-lg font-medium duration-300">
+              </Link>
+
+              {/* Large Device Menu */}
+              <DropdownMenu>
+                <DropdownMenuTrigger className="text-feed-black border-feed-black hover:bg-feed-lime flex h-10 cursor-pointer items-center gap-1 rounded-full border-[1.9px] bg-transparent px-2.5 text-lg font-medium caret-neutral-50 outline-0 duration-300">
                   {user ? (
                     <>
-                      <TbLayoutDashboard className="text-feed-jungle group-hover:text-feed-lime size-5.5" />
-                      <Link
-                        href="/dashboard"
-                        className="text-feed-jungle group-hover:text-feed-lime"
-                      >
-                        Dashboard
-                      </Link>
+                      <MdOutlineAccountCircle className="size-5" />
+                      <span>Account</span>
                     </>
                   ) : (
                     <>
-                      <CgLogIn className="text-feed-jungle group-hover:text-feed-lime size-5.5" />
-                      <Link
-                        href="/login"
-                        className="text-feed-jungle group-hover:text-feed-lime"
-                      >
-                        Sign In
-                      </Link>
+                      <span>Get Started</span>{" "}
+                      <FaArrowRightLong className="ml-1" />
                     </>
                   )}
-                </DropdownMenuItem>
-                <DropdownMenuItem className="bg-feed-jungle hover:!bg-feed-black w-ful h-9 rounded-[11px] text-lg font-medium duration-300">
-                  {user ? (
-                    <>
-                      <CgLogOut className="text-feed-lime size-5.5" />
-                      <span onClick={handleLogout} className="text-feed-lime">
-                        Sign Out
-                      </span>
-                    </>
-                  ) : (
-                    <>
-                      <MdOutlineAppRegistration className="text-feed-lime size-5.5" />
-                      <Link href="/register" className="text-feed-lime">
-                        Sign Up
-                      </Link>
-                    </>
-                  )}
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="border-feed-black w-fit space-y-1 rounded-2xl border">
+                  <DropdownMenuItem className="bg-feed-lime/50 hover:!bg-feed-jungle group w-ful h-9 rounded-[11px] text-lg font-medium duration-300">
+                    {user ? (
+                      <>
+                        <TbLayoutDashboard className="text-feed-jungle group-hover:text-feed-lime size-5.5" />
+                        <Link
+                          href="/dashboard"
+                          className="text-feed-jungle group-hover:text-feed-lime"
+                        >
+                          Dashboard
+                        </Link>
+                      </>
+                    ) : (
+                      <>
+                        <CgLogIn className="text-feed-jungle group-hover:text-feed-lime size-5.5" />
+                        <Link
+                          href="/login"
+                          className="text-feed-jungle group-hover:text-feed-lime"
+                        >
+                          Sign In
+                        </Link>
+                      </>
+                    )}
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="bg-feed-jungle hover:!bg-feed-black w-ful h-9 rounded-[11px] text-lg font-medium duration-300">
+                    {user ? (
+                      <>
+                        <CgLogOut className="text-feed-lime size-5.5" />
+                        <span onClick={handleLogout} className="text-feed-lime">
+                          Sign Out
+                        </span>
+                      </>
+                    ) : (
+                      <>
+                        <MdOutlineAppRegistration className="text-feed-lime size-5.5" />
+                        <Link href="/register" className="text-feed-lime">
+                          Sign Up
+                        </Link>
+                      </>
+                    )}
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+
+            {/* Mobile menu button */}
+            <div className="flex items-center md:hidden">
+              {/* Mobile Cart Icon */}
+              <Link href="/cart" className="relative mr-4">
+                <ShoppingCart className="h-6 w-6 text-gray-700" />
+                {cartItemCount > 0 && (
+                  <span className="absolute -top-1 -right-1 flex h-5 w-5 items-center justify-center rounded-full bg-[#FF0000] text-xs text-white">
+                    {cartItemCount}
+                  </span>
+                )}
+              </Link>
+
+              <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:ring-2 focus:ring-[#FF0000] focus:outline-none focus:ring-inset"
+              >
+                <span className="sr-only">Open main menu</span>
+                {isMenuOpen ? (
+                  <X className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <Menu className="block h-6 w-6" aria-hidden="true" />
+                )}
+              </button>
+            </div>
           </div>
+        </div>
 
-          {/* Mobile menu button */}
-          <div className="flex items-center md:hidden">
-            {/* Mobile Cart Icon */}
-            <Link href="/cart" className="relative mr-4">
-              <ShoppingCart className="h-6 w-6 text-gray-700" />
-              {cartItemCount > 0 && (
-                <span className="absolute -top-1 -right-1 flex h-5 w-5 items-center justify-center rounded-full bg-[#FF0000] text-xs text-white">
-                  {cartItemCount}
-                </span>
-              )}
-            </Link>
+        {/* Mobile menu */}
+        {isMenuOpen && (
+          <div className="md:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3 bg-[#D6F599] h-screen w-2/4 shadow-2xl">
+              <Link
+                href="/find-meals"
+                className="hover:text-feed-lime block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Find Meals
+              </Link>
+              <Link
+                href="/about"
+                className="hover:text-feed-lime block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                About
+              </Link>
 
-            <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:ring-2 focus:ring-[#FF0000] focus:outline-none focus:ring-inset"
-            >
-              <span className="sr-only">Open main menu</span>
-              {isMenuOpen ? (
-                <X className="block h-6 w-6" aria-hidden="true" />
+              <Link
+                href="/contact"
+                className="hover:text-feed-lime block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Contact Us
+              </Link>
+              {!user ? (
+                <>
+                  <Link
+                    href="/dashboard"
+                    className="hover:text-feed-lime block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    Dashboard
+                  </Link>
+                  <button
+                    onClick={handleLogout}
+                    className="hover:text-feed-lime w-full rounded-md px-3 py-2 text-left text-base font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    Sign Out
+                  </button>
+                </>
               ) : (
-                <Menu className="block h-6 w-6" aria-hidden="true" />
+                <>
+                  <Link
+                    href="/login"
+                    className="hover:text-feed-lime block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    Sign In
+                  </Link>
+                  <Link
+                    href="/register"
+                    className="hover:text-feed-lime block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    Sign Up
+                  </Link>
+                </>
               )}
-            </button>
+            </div>
           </div>
-        </div>
-      </div>
-
-      {/* Mobile menu */}
-      {isMenuOpen && (
-        <div className="md:hidden">
-          <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-            <Link
-              href="/find-meals"
-              className="hover:text-feed-lime block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              Find Meals
-            </Link>
-            <Link
-              href="/about"
-              className="hover:text-feed-lime block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              About
-            </Link>
-
-            <Link
-              href="/contact"
-              className="hover:text-feed-lime block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              Contact Us
-            </Link>
-            {!user ? (
-              <>
-                <Link
-                  href="/dashboard"
-                  className="hover:text-feed-lime block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"
-                  onClick={() => setIsMenuOpen(false)}
-                >
-                  Dashboard
-                </Link>
-                <button
-                  onClick={handleLogout}
-                  className="hover:text-feed-lime w-full rounded-md px-3 py-2 text-left text-base font-medium text-gray-700 hover:bg-gray-50"
-                >
-                  Sign Out
-                </button>
-              </>
-            ) : (
-              <>
-                <Link
-                  href="/login"
-                  className="hover:text-feed-lime block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"
-                  onClick={() => setIsMenuOpen(false)}
-                >
-                  Sign In
-                </Link>
-                <Link
-                  href="/register"
-                  className="hover:text-feed-lime block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"
-                  onClick={() => setIsMenuOpen(false)}
-                >
-                  Sign Up
-                </Link>
-              </>
-            )}
-          </div>
-        </div>
-      )}
-    </nav>
+        )}
+      </nav>
+    </header>
   );
 };
 
